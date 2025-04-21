df7ce2ba9569495243b1eec6b9d948e5da83d55f
pavanthatipamula6 <58951355+pavanthatipamula6@users.noreply.github.com>
2025-04-21 13:30:04 +0530

    Update thingSpeakEsp01.h

--- thingSpeakEsp01.h
+++ thingSpeakEsp01.h
@@ -1,5 +1,5 @@
-#ifndef ESP01_H
-#define ESP01_H
+#ifndef THINGSPEAK_ESP01_H
+#define THINGSPEAK_ESP01_H
 
 #include <SoftwareSerial.h>
 
@@ -8,17 +8,16 @@
 #define espBaud 115200
 #define DEBUG true
 
-extern String mySSID;
-extern String myPWD;
-extern String myAPI;
-extern String myHOST;
-extern String myPORT;
+extern String esp01SSID;
+extern String esp01PWD;
+extern String esp01WriteAPI;
+extern String esp01HOST;
+extern String esp01PORT;
 
-extern SoftwareSerial espSerial;
+extern SoftwareSerial espSerial;  // only declared, not defined here
 
 void initEsp01();
 void sendThingSpeak(int fieldNumber = 0, String data = "test");
-// String espData(String command, const int timeout, boolean debug);
 String espData(String command, const int timeout, bool debug);
 
 #endif
